dffe0140a2ff0a5547d0405f34c9fdc3051489b6
Anton Shlikhta <suddyadred@gmail.com>
2016-10-16 01:05:40 +0300

    suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -7,5 +7,31 @@ $(document).ready(function() {
         $(document.body).toggleClass('overflow');
     });
 
+    // for help
+        $('span.jQtooltip').each(function() {
+            var el = $(this),
+                title = el.attr('title');
+            if (title && title != '') {
+                el.attr('title', '').append('<div>' + title + '</div>');
+                var width = el.find('div').width();
+                var height = el.find('div').height();
+                el.hover(
+                    function() {
+                        el.find('div')
+                            .clearQueue()
+                            .delay(200)
+                            .animate({width: width + 20, height: height + 20}, 200).show(200)
+                            .animate({width: width, height: height}, 200);
+                    },
+                    function() {
+                        el.find('div')
+                            .animate({width: width + 20, height: height + 20}, 250)
+                            .animate({width: 'hide', height: 'hide'}, 250);
+                    }
+                ).mouseleave(function() {
+                        if (el.children().is(':hidden')) el.find('div').clearQueue();
+                    });
+            }
+        });
 
 });
