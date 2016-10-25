fb6387db5499ef31867fd6fd0c37ab90fa7df634
Anton Shlikhta <suddyadred@gmail.com>
2016-10-25 22:07:52 +0300

    suddya

--- js/m-mainJs.js
+++ js/m-mainJs.js
@@ -23,4 +23,22 @@ $(document).ready(function() {
     $(".accordion-list > li > h2").on('click', function(){
         $(this).toggleClass('active').parent().find('.accordion-box-content').slideToggle(500);
     });
+
+    //delete input wrap
+    $('.delete-wrap').unwrap();
+
+    //input style
+    $( ".input__field--hoshi").each(function(){
+        $(this).change(function() {
+            if($(this).val() != ''){
+                // ≈сли поле не пустое удал€ем класс-указание
+                $(this).parent().addClass('input__field');
+                $(this).parent().find('.input__label-content').addClass('active__field');
+            } else {
+                // ≈сли поле пустое добавл€ем класс-указание
+                $(this).parent().removeClass('input__field');
+                $(this).parent().find('.input__label-content').removeClass('active__field');
+            }
+        });
+    });
 });
