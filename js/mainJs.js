48b991a87015218867578799f61474660474880e
Anton Shlikhta <suddyadred@gmail.com>
2016-11-05 23:42:37 +0200

    suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -10,6 +10,16 @@ $(document).ready(function() {
     //     $('.flex-prev').empty();
     //     $('.flex-next').empty();
 
+    $('.mobile-menu-toggle, .mobile-menu-overlay').on('click', function(){
+        $('.mobile-menu-toggle').toggleClass('active');
+        $('.mobile-menu-wrap').toggleClass('showing');
+        $(document.body).toggleClass('overflow');
+    });
+
+    $(".mobile-menu a").on('click', function(){
+        $(this).toggleClass('open').siblings('.sub-menu').slideToggle(350);
+    });
+
     // the blocking of links default behavior
     function prevent(){
         $('.prevent, .mobile-menu .expanded, a[href="#"]').on('click', function(event){
